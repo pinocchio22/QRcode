74e41a6985f581aaabf28ca465c50297ce3a141b
pinocchio22 <vviian.2@gmail.com>
2022-06-02 20:22:40 +0900

    view binding

--- app/src/main/java/com/example/qrcode/MainActivity.kt
+++ app/src/main/java/com/example/qrcode/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.qrcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.qrcode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding  // 바인딩 변수
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // 뷰 바인딩 설정
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
+
+
 }
