58960b712dcabd848f74c1131f4fb911602b7ebc
pinocchio22 <vviian.2@gmail.com>
2022-06-02 20:30:32 +0900

    MainActivity.kt

--- app/src/main/java/com/example/qrcode/MainActivity.kt
+++ app/src/main/java/com/example/qrcode/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.qrcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.example.qrcode.databinding.ActivityMainBinding
+import com.google.common.util.concurrent.ListenableFuture
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding  // 바인딩 변수
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +21,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        startCamera()
     }
 
+    // 미리보기와 이미지 분석 시작
+    fun startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = getPreview()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        }, ContextCompat.getMainExecutor(this))
+
+    }
+
+    // 미리보기 객체 반환
+    fun getPreview() : Preview {
+        val preview : Preview = Preview.Builder().build()   // Preview 객체 생성
+        preview.setSurfaceProvider(binding.barcodePreview.getSurfaceProvider())
+        return preview
+    }
 }
