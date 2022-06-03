b8fa3b41cd96d91c5e4dfccaa4e932236e03b280
pinocchio22 <vviian.2@gmail.com>
2022-06-03 17:15:05 +0900

    completed preview

--- app/src/main/java/com/example/qrcode/MainActivity.kt
+++ app/src/main/java/com/example/qrcode/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.qrcode
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
@@ -14,6 +18,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding  // 바인딩 변수
     private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
 
+    private val PERMISSIONS_REQUEST_CODE = 1
+    private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,7 +29,32 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        startCamera()
+        if (!hasPermissions(this)) {
+            requestPermissions(PERMISSIONS_REQUIRED, PERMISSIONS_REQUEST_CODE)
+        } else {
+            startCamera()
+        }
+    }
+
+    // 카메라 권한 유무 확인
+    fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+        ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    // 권한 요청 콜백 함수
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()) {
+                Toast.makeText(this@MainActivity, "권한 요청이 승인 되었습니다.", Toast.LENGTH_LONG).show()
+                startCamera()
+            } else {
+                Toast.makeText(this@MainActivity, "권한 요청이 거부되었습니다.", Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
     }
 
     // 미리보기와 이미지 분석 시작
--- app/src/main/java/com/example/qrcode/ResultActivity.kt
+++ app/src/main/java/com/example/qrcode/ResultActivity.kt
@@ -1,10 +1,12 @@
 package com.example.qrcode
 
+import androidx.appcompat.app.AppCompatActivity
+
 /**
  * @author CHOI
  * @email vviian.2@gmail.com
  * @created 2022-06-02
  * @desc
  */
-class ResultActivity {
+class ResultActivity : AppCompatActivity {
 }
